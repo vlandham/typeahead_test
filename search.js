8e302c9e53209fc2e1c1830b24544e754612dcc3
Jim V <vlandham@gmail.com>
2015-02-23 15:15:24 -0800

    initial

---
+++ search.js
@@ -0,0 +1,54 @@
+
+
+var movies = new Bloodhound({
+  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+  prefetch: 'data/movies_all_compressed.json'
+});
+
+movies.initialize();
+
+var tropes = new Bloodhound({
+  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+  prefetch: 'data/tropes_all_compressed.json'
+});
+
+tropes.initialize();
+ 
+$('#search .typeahead').typeahead({
+  hint: true,
+  highlight: true,
+  minLength: 1
+},
+{
+  name: 'tropes',
+  displayKey: 'name',
+  source: tropes.ttAdapter(),
+  templates: {
+    header: '<h3 class="search-name">Tropes</h3>'
+  }
+},
+{
+  name: 'movies',
+  displayKey: 'name',
+  source: movies.ttAdapter(),
+  templates: {
+    header: '<h3 class="search-name">Movies</h3>'
+  }
+});
+
+$('.typeahead').on('typeahead:selected', function(el,sug,data) {
+  console.log(sug);
+  $("#data").html("<h3>Returns:</h3>" + "<code>" + JSON.stringify(sug) + "</code>");
+  $('.typeahead').blur();
+});
+
+// $('.typeahead').on('typeahead:autocompleted', function(el,sug,data) {
+//   console.log('suggestions');
+//   console.log(sug);
+// });
+
+$('.typeahead').focus(function (e) {
+  $('.typeahead').typeahead('val',''); // Clear typeahead
+});
